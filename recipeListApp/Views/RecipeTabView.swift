d7f547a515b89c2307e737f6eafb670656c9f145
Elina <elina.zekunde@gmail.com>
2021-03-10 12:59:03 +0200

    add tab view

---
+++ recipeListApp/Views/RecipeTabView.swift
@@ -0,0 +1,35 @@
+//
+//  RecipeTabView.swift
+//  recipeListApp
+//
+//  Created by Elīna Zekunde on 10/03/2021.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        TabView {
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
